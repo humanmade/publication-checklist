6368aade93db696009a432333303392762112b29
Ryan McCue <me@ryanmccue.info>
2019-07-11 12:39:32 +0100

    Give hide/show button some space

--- src/components/ChecklistPanelContent.js
+++ src/components/ChecklistPanelContent.js
@@ -69,6 +69,7 @@ const ChecklistPanelContent = ( {
 
 			{ isComplete && (
 				<Button
+					className={ `${ baseClassName }__toggle-completed` }
 					isLink={ true }
 					type="button"
 					onClick={ () => setExpanded( ! isExpanded ) }
